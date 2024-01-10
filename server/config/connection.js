5f89fe8f61b4977e4e959c21a6ed9a27506ca38e
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-01-10 16:27:48 -0600

    heroku deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/carRentalDB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/carRentalDB.');
+
 
 module.exports = mongoose.connection;
